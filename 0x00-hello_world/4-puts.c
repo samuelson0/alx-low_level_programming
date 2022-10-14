2c153153b9f0f1699bff876cabf9f7e8bcbf7e28
samuelson0 <mailwilson@gmail.com>
2022-10-14 07:25:55 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,6 +3,6 @@
 /* Return: Always 0 (Success) */
 int main(void)
 {
-	puts("\"Programming is line building a multilingual puzzle"\n);
+	puts("\"Programming is line building a multilingual puzzle");
 	return (0);
 }
